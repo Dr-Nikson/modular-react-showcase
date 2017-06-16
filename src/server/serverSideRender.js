0ea38e24442eac53e83fb7d50a7a9b5e0492bb02
Nik NIk <razmes-75@mail.ru>
2017-06-16 17:48:11 +0300

    Squashed commit of the following:  - line separators test

--- src/server/serverSideRender.js
+++ src/server/serverSideRender.js
@@ -5,12 +5,13 @@ import ReactDOMServer from 'react-dom/server'
 import App from 'client/App'
 
 import type { $Request, $Response } from 'express'
-import Template from "./Template"
+import Template from './Template'
 
+export const rendererFactory = (template: Template) => {
+  return (req: $Request, res: $Response): void => {
+    const someElse = ReactDOMServer.renderToString(<App />)
+    const output = template.templateString.replace('{{html}}', someElse)
 
-export const rendererFactory = (template: Template) => (req: $Request, res: $Response): void => {
-  const someElse = ReactDOMServer.renderToString(<App />)
-  const output = template.templateString.replace('{{html}}', someElse)
-  res.set('Content-Type', 'text/html').send(output)
+    res.set('Content-Type', 'text/html').send(output)
+  }
 }
-
